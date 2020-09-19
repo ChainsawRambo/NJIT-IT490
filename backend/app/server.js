6ab1f65ed4b12008a6dae1295dd70e56f7017d1d
Michael Drozdowski <mjd76@njit.edu>
2020-09-19 12:01:57 -0400

    Fix: updated server.js

--- backend/app/server.js
+++ backend/app/server.js
@@ -10,10 +10,39 @@ const queue = 'requests';
 const messaging_url = `amqp://${messaging_user}:${messaging_pass}@${messaging_host}`;
 const amqp = require('amqplib/callback_api');
 
-//Talk with message
+// Function to send response
 function send_response(channel, msg, response) {
     channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(response)), {
         correlationId: msg.properties.correlationId
     });
     channel.ack(msg);
 }
+
+// Communications with Message
+amqp.connect(messaging_url, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.prefetch(1);
+        console.log('Listening for requests...');
+        channel.consume(queue, function reply(msg) {
+            console.log(`Received: ${msg.content}`);
+            var received_json = JSON.parse(msg.content);
+            var action = received_json['action'];
+            switch (action) {
+                case 'ECHO':
+                    data = received_json['data'];
+                    console.log(`Echoing ${data}`);
+                    send_response(channel, msg, {'status': 'OK', 'data': data});
+                    break;
+            }
+        });
+    });
+});
