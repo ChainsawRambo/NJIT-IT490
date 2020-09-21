e8a26ba225decc32369349126b8cd5dabfb89cd7
Michael Drozdowski <mjd76@njit.edu>
2020-09-21 16:31:57 -0400

    Fix: filed out app.py code

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
 #Imports
 import pika
 import time
+import logging
+
+#Sleep to allow other services to start first
+time.sleep(20)
 
 #Back End Code
-
-
+logging.info("Back End Is Running Now")
+#    credentials = pika.PlainCredentials('guest','guest')
+#    connection = pika.BlockingConnection(
+#       pika.ConnectionParameters(
+#           host='messaging',
+#           credentials=credentials
+#       )
+#    )
