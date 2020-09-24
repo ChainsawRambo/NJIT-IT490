d78aeaa21a46a7f2fd7147a6e58236f08bd5de78
Michael Drozdowski <mjd76@njit.edu>
2020-09-24 13:28:25 -0400

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
 #Imports
 import pika
 import time
-import logging
+
+print("TEST Back End Is Running Now")
 
 #Sleep to allow other services to start first
 time.sleep(20)
 
 #Back End Code
 print("Back End Is Running Now")
-#    credentials = pika.PlainCredentials('guest','guest')
-#    connection = pika.BlockingConnection(
-#       pika.ConnectionParameters(
+credentials = pika.PlainCredentials('guest','guest')
+#connection = pika.BlockingConnection(
+#	pika.ConnectionParameters(
 #           host='messaging',
 #           credentials=credentials
 #       )
-#    )
+#)
