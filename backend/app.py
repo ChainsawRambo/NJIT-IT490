2ff81ecea03dabaf8f469e7f562ec62bf5f32359
Michael Drozdowski <mjd76@njit.edu>
2020-09-24 14:23:03 -0400

    Removed second print statement

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,12 @@
 import pika
 import time
 
-print("TEST Back End Is Running Now")
-
 #Sleep to allow other services to start first
 time.sleep(20)
 
 #Back End Code
 print("Back End Is Running Now")
-credentials = pika.PlainCredentials('guest','guest')
+#credentials = pika.PlainCredentials('guest','guest')
 #connection = pika.BlockingConnection(
 #	pika.ConnectionParameters(
 #           host='messaging',
