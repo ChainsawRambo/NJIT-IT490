8334c5846176598dc1c6d3c3e8edbfd7efe23896
Kamal Youssef <keemz@Kamals-MacBook-Pro.local>
2020-09-29 11:06:27 -0400

    Feature: Rabbitmq FE Setup

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,31 @@
 from flask import Flask
+import pika
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello From Front End'
+def index():
+    return 'OK'
+
+
+@app.route('/add-job/<cmd>')
+def add(cmd):
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='messaging', credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=cmd,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+    return " [x] Sent: %s" % cmd
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
