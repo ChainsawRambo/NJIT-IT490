9e532ab8dbeaeeebd324d1851f2ea8f5a0ff39a0
Michael Drozdowski <mjd76@njit.edu>
2020-10-15 14:44:54 -0400

    Added capability to search via ingredient

--- backend/app.py
+++ backend/app.py
@@ -91,11 +91,16 @@ def callback(ch, method, properties, body):
                 cursor.execute('INSERT INTO usersinfo VALUES (%s, %s, %s, %s, %s);', (username, firstname, lastname, email, hashed))
                 conn.commit()
                 response = {'success': True}
+        #scraping
         elif action == 'SCRAPE':
             data = request['data']
+            #cocktailname
             cocktailname = {'s':data['searchfield']}
+            #ingredients
+            cocktailI = {'i':data['searchfield']}
             r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php', params=cocktailname)
-            response = r.json()
+            i = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php', params=cocktailI)
+            response = r.json() + i.json()
         else:
             response = {'success': False, 'message': "Unknown action"}
     logging.info(response)
