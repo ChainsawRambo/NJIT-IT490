1d6302cef67c2cdeab50f5288401f9ade4ab0513
Kamal Youssef <keemz@kamals-mbp.resource.campus.njit.edu>
2020-10-22 13:41:49 -0400

    Adding CSS to userpage

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ time.sleep(sleepTime)
 # Connect with Messaging
 print(' [*] Connecting to server ...')
 credentials = pika.PlainCredentials(os.environ['RABBITMQ_DEFAULT_USER'],
-                                        os.environ['RABBITMQ_DEFAULT_PASS'])
+                                    os.environ['RABBITMQ_DEFAULT_PASS'])
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='messaging', credentials=credentials))
 channel = connection.channel()
@@ -33,30 +33,15 @@ try:
         password=postgres_password
     )
 
-    '''
-    postgres_insert_query = """ INSERT INTO usersinfo (username, firstname, lastname, email, registration_date, hash) VALUES ( %s,%s, %s, %s, %s, CURRENT_TIMESTAMP, %s)  """
-    record_to_insert = ('ky98', 'Kamal', 'Youssef',
-                        'testemail4324343@gmail.com', 'password123', 'd3fmsdf21342343sd3ksekfl')
 
-    cursor.execute(postgres_insert_query, record_to_insert)
-
-    conn.commit()
-    count = cursor.rowcount
-    print(count, "Record inserted successfully into usersinfo table")
-    '''
 except (Exception, psycopg2.Error) as error:
     if(connection):
         print("Failed to insert record into usersinfo table", error)
-'''
-finally:
-    # closing database connection.
-    if(conn):
-        cursor.close()
-        conn.close()
-        print("PostgreSQL connection is closed")
-'''
+
 cursor = conn.cursor()
 # Talking with Messaging
+
+
 def callback(ch, method, properties, body):
     request = json.loads(body)
     if 'action' not in request:
@@ -70,8 +55,9 @@ def callback(ch, method, properties, body):
             data = request['data']
             username = data['username']
             logging.info(f"GETHASH request for {username} received")
-            cursor.execute('SELECT hash FROM usersinfo WHERE username=%s;', (username,))
-            row =  cursor.fetchone()
+            cursor.execute(
+                'SELECT hash FROM usersinfo WHERE username=%s;', (username,))
+            row = cursor.fetchone()
             if row == None:
                 response = {'success': False}
             else:
@@ -79,69 +65,84 @@ def callback(ch, method, properties, body):
         elif action == 'REGISTER':
             data = request['data']
             firstname = data['firstname']
-            lastname = data ['lastname']
+            lastname = data['lastname']
             email = data['email']
             username = data['username']
             hashed = data['hash']
             logging.info(f"REGISTER request for {email} received")
-            cursor.execute('SELECT * FROM usersinfo WHERE email=%s or username=%s;', (email,username))
+            cursor.execute(
+                'SELECT * FROM usersinfo WHERE email=%s or username=%s;', (email, username))
             if cursor.fetchone() != None:
-                response = {'success': False, 'message': 'Username or email already exists'}
+                response = {'success': False,
+                            'message': 'Username or email already exists'}
             else:
-                cursor.execute('INSERT INTO usersinfo VALUES (%s, %s, %s, %s, %s);', (username, firstname, lastname, email, hashed))
+                cursor.execute('INSERT INTO usersinfo VALUES (%s, %s, %s, %s, %s);',
+                               (username, firstname, lastname, email, hashed))
                 conn.commit()
                 response = {'success': True}
         elif action == 'SCRAPE':
             data = request['data']
             searchby = data['dropdownvalue']
             if searchby == 'cocktail_name':
-                searchbycocktailname = {'s':data['searchfield']}
-                r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php', params=searchbycocktailname)
+                searchbycocktailname = {'s': data['searchfield']}
+                r = requests.get(
+                    'https://www.thecocktaildb.com/api/json/v1/1/search.php', params=searchbycocktailname)
                 r = r.json()
                 if r['drinks']:
-                    cocktailname=r['drinks'][0]['strDrink']
-                    cocktailimage=r['drinks'][0]['strDrinkThumb']
-                    cocktailingredients=[]
-                    cocktailmeasurements=[]
-                    for i in range(1,16):
+                    cocktailname = r['drinks'][0]['strDrink']
+                    cocktailimage = r['drinks'][0]['strDrinkThumb']
+                    cocktailingredients = []
+                    cocktailmeasurements = []
+                    for i in range(1, 16):
                         if r['drinks'][0]['strIngredient' + str(i)]:
-                            cocktailingredients.append(r['drinks'][0]['strIngredient' + str(i)])
-                    for i in range(1,16):
+                            cocktailingredients.append(
+                                r['drinks'][0]['strIngredient' + str(i)])
+                    for i in range(1, 16):
                         if r['drinks'][0]['strMeasure' + str(i)]:
-                            cocktailmeasurements.append(r['drinks'][0]['strMeasure' + str(i)])
-                    cocktailinstructions=r['drinks'][0]['strInstructions']
-                    cocktailcategory=r['drinks'][0]['strCategory']
-                    response = {'success':True, 'search':'cocktail_name', 'cocktailname': cocktailname, 'cocktailimage': cocktailimage, 'cocktailingredients':cocktailingredients, 'cocktailmeasurements':cocktailmeasurements, 'cocktailinstructions':cocktailinstructions, 'cocktailcategory':cocktailcategory}
+                            cocktailmeasurements.append(
+                                r['drinks'][0]['strMeasure' + str(i)])
+                    cocktailinstructions = r['drinks'][0]['strInstructions']
+                    cocktailcategory = r['drinks'][0]['strCategory']
+                    response = {'success': True, 'search': 'cocktail_name', 'cocktailname': cocktailname, 'cocktailimage': cocktailimage, 'cocktailingredients': cocktailingredients,
+                                'cocktailmeasurements': cocktailmeasurements, 'cocktailinstructions': cocktailinstructions, 'cocktailcategory': cocktailcategory}
                 else:
-                    response = {'success': False, 'message': "Invalid Cocktail Name"}
+                    response = {'success': False,
+                                'message': "Invalid Cocktail Name"}
             elif searchby == 'ingredient_name':
-                searchbyingredientname = {'i':data['searchfield']}
-                r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php', params=searchbyingredientname)
+                searchbyingredientname = {'i': data['searchfield']}
+                r = requests.get(
+                    'https://www.thecocktaildb.com/api/json/v1/1/search.php', params=searchbyingredientname)
                 r = r.json()
                 if r['ingredients']:
-                    ingredientname=r['ingredients'][0]['strIngredient']
-                    ingredientdescription=r['ingredients'][0]['strDescription']
-                    ingredienttype=r['ingredients'][0]['strType']
-                    ingredientalchohol=r['ingredients'][0]['strAlcohol']
-                    response = {'success':True, 'search':'ingredient_name', 'ingredientname': ingredientname, 'ingredientdescription' : ingredientdescription, 'ingredienttype': ingredienttype, 'ingredientalchohol': ingredientalchohol}
+                    ingredientname = r['ingredients'][0]['strIngredient']
+                    ingredientdescription = r['ingredients'][0]['strDescription']
+                    ingredienttype = r['ingredients'][0]['strType']
+                    ingredientalchohol = r['ingredients'][0]['strAlcohol']
+                    response = {'success': True, 'search': 'ingredient_name', 'ingredientname': ingredientname,
+                                'ingredientdescription': ingredientdescription, 'ingredienttype': ingredienttype, 'ingredientalchohol': ingredientalchohol}
                 else:
-                    response = {'success': False, 'message': "Invalid Ingredient Name"}
+                    response = {'success': False,
+                                'message': "Invalid Ingredient Name"}
             elif searchby == 'random':
-                r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-                r=r.json()
-                cocktailname=r['drinks'][0]['strDrink']
-                cocktailimage=r['drinks'][0]['strDrinkThumb']
-                cocktailingredients=[]
-                cocktailmeasurements=[]
-                for i in range(1,16):
+                r = requests.get(
+                    'https://www.thecocktaildb.com/api/json/v1/1/random.php')
+                r = r.json()
+                cocktailname = r['drinks'][0]['strDrink']
+                cocktailimage = r['drinks'][0]['strDrinkThumb']
+                cocktailingredients = []
+                cocktailmeasurements = []
+                for i in range(1, 16):
                     if r['drinks'][0]['strIngredient' + str(i)]:
-                        cocktailingredients.append(r['drinks'][0]['strIngredient' + str(i)])
-                for i in range(1,16):
+                        cocktailingredients.append(
+                            r['drinks'][0]['strIngredient' + str(i)])
+                for i in range(1, 16):
                     if r['drinks'][0]['strMeasure' + str(i)]:
-                        cocktailmeasurements.append(r['drinks'][0]['strMeasure' + str(i)])
-                cocktailinstructions=r['drinks'][0]['strInstructions']
-                cocktailcategory=r['drinks'][0]['strCategory']
-                response = {'success':True, 'search':'random', 'cocktailname': cocktailname, 'cocktailimage': cocktailimage, 'cocktailingredients':cocktailingredients, 'cocktailmeasurements':cocktailmeasurements, 'cocktailinstructions':cocktailinstructions, 'cocktailcategory':cocktailcategory}
+                        cocktailmeasurements.append(
+                            r['drinks'][0]['strMeasure' + str(i)])
+                cocktailinstructions = r['drinks'][0]['strInstructions']
+                cocktailcategory = r['drinks'][0]['strCategory']
+                response = {'success': True, 'search': 'random', 'cocktailname': cocktailname, 'cocktailimage': cocktailimage, 'cocktailingredients': cocktailingredients,
+                            'cocktailmeasurements': cocktailmeasurements, 'cocktailinstructions': cocktailinstructions, 'cocktailcategory': cocktailcategory}
         else:
             response = {'success': False, 'message': "Unknown action"}
     logging.info(response)
@@ -151,6 +152,8 @@ def callback(ch, method, properties, body):
         body=json.dumps(response)
     )
 
+
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='request', auto_ack=True, on_message_callback=callback)
+channel.basic_consume(queue='request', auto_ack=True,
+                      on_message_callback=callback)
 channel.start_consuming()
