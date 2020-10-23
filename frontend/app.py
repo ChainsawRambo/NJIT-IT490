79fd7244e867cc14be8897b1cf34f3817a309f33
Kamal Youssef <keemz@Kamals-MBP.hsd1.nj.comcast.net>
2020-10-23 13:21:31 -0400

    Added Bootstrap and basic CSS

--- frontend/app.py
+++ frontend/app.py
@@ -7,13 +7,14 @@ import os
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def loginpage():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         msg = messaging.Messaging()
-        msg.send('GETHASH', { 'username': username })
+        msg.send('GETHASH', {'username': username})
         response = msg.receive()
         if response['success'] != True:
             return "Login failed."
@@ -24,7 +25,8 @@ def loginpage():
             return "Login failed."
     return render_template('login.html')
 
-@app.route('/userpage', methods=['GET','POST'])
+
+@app.route('/userpage', methods=['GET', 'POST'])
 def userpage():
     if 'username' not in session:
         return redirect('/')
@@ -35,8 +37,8 @@ def userpage():
         msg.send(
             'SCRAPE',
             {
-                'searchfield' : searchfield,
-                'dropdownvalue' : dropdownvalue
+                'searchfield': searchfield,
+                'dropdownvalue': dropdownvalue
             }
         )
         response = msg.receive()
@@ -53,9 +55,12 @@ def userpage():
                 return render_template('userpage.html', thumbnail=thumbnail)
         else:
             flash("error")
-    return render_template('userpage.html')
+        print(session['username'])
+        username = session['username']
+    return render_template('userpage.html', usernm=session['username'])
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def registerpage():
     if request.method == 'POST':
         firstname = request.form['firstname']
@@ -69,10 +74,10 @@ def registerpage():
             msg.send(
                 'REGISTER',
                 {
-                    'firstname' : firstname,
-                    'lastname' : lastname,
+                    'firstname': firstname,
+                    'lastname': lastname,
                     'email': email,
-                    'username' : username,
+                    'username': username,
                     'hash': generate_password_hash(password)
                 }
             )
@@ -83,6 +88,7 @@ def registerpage():
             else:
                 return f"{response['message']}"
     return render_template('register.html')
+
 
 @app.route('/logout')
 def logout():
